fd5d961a1d168bf05e1e3e635d48a25a4527ebc2
yogeshyogi1721 <yogeshyogesh2102006@gmail.com>
2024-12-12 15:53:13 +0000

    pp1

--- Practical1.java
+++ Practical1.java
@@ -1,38 +1,36 @@
 class Person
 {
-  String firstName;
-  String lastName;
-  int age;
-
-    Person()
-   {
-        firstName= "";
-        lastName= "";
-        age=0;
-    }
-    Person (String firstName, String lastName,int age) 
-    {
-        this.firstName=firstName;
-        this.lastName=lastName;
-        this.age=age;
-    }
-    
-     String getFullName() 
-    {
-        return firstName + "" + lastName;
-    }
-}
-class Practical1
+String firstName;
+String lastName;
+int age;
+// Default constructor
+public Person()
 {
-    public static void main(String[] args)
-    {
-    
-        Person person1 =new Person("John","Doe", 30);
-        Person person2 =new Person("Alice", "Smith",25);
-        System.out.println("Person 1:" + person1.getFullName());
-        System.out.println("Person 2:" + person2.getFullName());
-        double averageAge=(person1.age+ person2.age) /2.0;
-        System.out.println("Average Age: " + averageAge);
-    }
-}
+firstName = "";
+lastName = "";
+age = 0;
 
+}
+public Person(String firstName, String lastName, int age) {
+this.firstName = firstName;
+this.lastName = lastName;
+this.age = age;
+}
+// Method to get full name
+public String getFullName() {
+return firstName + " " + lastName;
+}
+}
+public class Practical1 
+{
+public static void main(String[] args)
+{
+// Create two Person objects
+Person person1 = new Person("John", "Doe", 30);
+Person person2 = new Person("Alice", "Smith", 25);
+System.out.println("Person 1: " + person1.getFullName());
+System.out.println("Person 2: " + person2.getFullName());
+double averageAge = (person1.age + person2.age) / 2.0;
+System.out.println("Average Age: " + averageAge);
+}
+}

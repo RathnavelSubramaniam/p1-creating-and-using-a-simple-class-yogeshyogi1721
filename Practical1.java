f13f2d3dd6fbcc16657a4049f76b00e2fa1ecb0f
yogeshyogi1721 <yogeshyogesh2102006@gmail.com>
2025-01-03 08:50:23 +0000

    1

--- Practical1.java
+++ Practical1.java
@@ -28,9 +28,9 @@ public static void main(String[] args)
 // Create two Person objects
 Person person1 = new Person("John", "Doe", 30);
 Person person2 = new Person("Alice", "Smith", 25);
-System.out.println("Person 1: " + person1.getFullName());
-System.out.println("Person 2: " + person2.getFullName());
+System.out.println("Person1: " + person1.getFullName());
+System.out.println("Person2: " + person2.getFullName());
 double averageAge = (person1.age + person2.age) / 2.0;
-System.out.println("Average Age: " + averageAge);
+System.out.println("Average age: " + averageAge);
 }
 }
